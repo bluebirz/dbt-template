ec61058fa48b3398a972af4a6c13f46a80211338
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-07-01 19:47:05 +0200

    add sqlfluff

--- dbt-bq/models/mart/weekly_avg_cases_per_country.sql
+++ dbt-bq/models/mart/weekly_avg_cases_per_country.sql
@@ -1,10 +1,9 @@
 select
-  date,
-  country_name,
-  avg(new_confirmed) over (
-    partition by country_name
-    order by date
-    rows between 6 preceding and current row
-  ) as weekly_avg_cases
-from {{ ref('covid_stats') }}
-where date between {{ var('start_date') }} and {{ var('end_date') }}
+    date,
+    country_name,
+    avg(new_confirmed) over (
+        partition by country_name order by date rows between 6 preceding and current row
+    ) as weekly_avg_cases
+from {{ ref("covid_stats") }}
+where date between {{ var("start_date") }} and {{ var("end_date") }}
+
--- dbt-bq/models/staging/covid_stats.sql
+++ dbt-bq/models/staging/covid_stats.sql
@@ -1,18 +1,19 @@
-{{ config(materialized='table') }}
+{{ config(materialized="table") }}
 
-with covid_data as (
-    select
-        date,
-        country_name,
-        cumulative_confirmed,
-        cumulative_recovered,
-        new_confirmed,
-        population,
-        cumulative_persons_fully_vaccinated,
-    from {{ source('covid19_open_data', 'covid19_open_data') }}
-)
+with
+    covid_data as (
+        select
+            date,
+            country_name,
+            cumulative_confirmed,
+            cumulative_recovered,
+            new_confirmed,
+            population,
+            cumulative_persons_fully_vaccinated,
+        from {{ source("covid19_open_data", "covid19_open_data") }}
+    )
 
-select * from covid_data
-{% if var("is_test_run", default=true) %}
-  limit 100
-{% endif %}
+select *
+from covid_data
+{% if var("is_test_run", default=true) %} limit 100 {% endif %}
+
