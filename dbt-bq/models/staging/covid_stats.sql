1a80cd605c04692c43b67c7e2f3738ff780dfe18
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-07-06 18:55:27 +0200

    update sql + fluffignore

--- dbt-bq/models/staging/covid_stats.sql
+++ dbt-bq/models/staging/covid_stats.sql
@@ -1,19 +1,19 @@
 {{ config(materialized="table") }}
 
 with
-    covid_data as (
-        select
-            date,
-            country_name,
-            cumulative_confirmed,
-            cumulative_recovered,
-            new_confirmed,
-            population,
-            cumulative_persons_fully_vaccinated,
-        from {{ source("covid19_open_data", "covid19_open_data") }}
-    )
+covid_data as (
+    select
+        date,
+        country_name,
+        cumulative_confirmed,
+        cumulative_recovered,
+        new_confirmed,
+        population,
+        cumulative_persons_fully_vaccinated
+    from {{ source("covid19_open_data", "covid19_open_data") }}
+)
 
 select *
 from covid_data
-{% if var("is_test_run", default=true) %} limit 100 {% endif %}
-
+{% if var("is_test_run", default=true) %} limit 100
+{% endif %}
